383623d50e1d5cf01280c8b7ca72956ac947181c
Bruno Rimolo <bruno.rimolo@gmail.com>
2020-08-23 23:59:36 -0300

    Novo branch para testes

--- src/model/Retangulo.java
+++ src/model/Retangulo.java
@@ -13,7 +13,10 @@ public class Retangulo {
         /* sobrecarga: metodos iguais tipos diferentes de parâmetros */
         public Retangulo(Double x1, Double y1, Double x2, Double y2, Double x3, Double y3, Double x4, Double y4) {
                 this.base = new Reta(x1,y1,x2,y2);
-                this.altura = new Reta(x3,y3,x4,y4);
+                Reta reta2 = new Reta(x3,y3,x4,y4);
+                Double tamanhoReta2 = reta2.getTamanho();
+                Double tamanhoReta1 = this.base.getTamanho();
+                
         }
     // Métodos de acesso
     public Reta getBase() {
