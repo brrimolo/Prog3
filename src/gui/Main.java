0af3f0d395e3592ea7c0bc00efa280dd4c4ed981
Bruno Rimolo <bruno.rimolo@gmail.com>
2020-08-24 23:09:04 -0300

    Adicionada GUI

---
+++ src/gui/Main.java
@@ -0,0 +1,20 @@
+package gui;
+
+/**
+ *
+ * @author brrimolo
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+  //      GuiPonto guiPonto = new GuiPonto();
+//        guiPonto.setVisible(true);
+        GuiRetangulo guiRetangulo = new GuiRetangulo();
+        guiRetangulo.setVisible(true);
+    }
+    
+}
